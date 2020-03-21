45c854550c7d9b9f7cbe5ef182db3415a375ac74
Marina <marina.delgado536@alu.uhu.es>
2020-03-21 17:57:09 +0100

    NUEVA VERSION DE llueve.pl
    Se agregan parámetros (lógica de predicados)

--- ejemplos_teoria/llueve.pl
+++ ejemplos_teoria/llueve.pl
@@ -2,5 +2,12 @@
 	EJEMPLO SECCION 0. INTRODUCCION
 **************************************************/
 
-llueve. % Hecho
-se_riegan_las_plantas :- llueve. % Regla
+% llueve. % Hecho
+% se_riegan_las_plantas :- llueve. % Regla
+
+% modificación
+
+llueve.
+se_riegan(tomates):-llueve.
+se_riegan(lechugas):-llueve.
+se_riegan(fresas):-llueve.
